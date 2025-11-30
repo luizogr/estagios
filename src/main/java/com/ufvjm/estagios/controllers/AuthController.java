5807f685ab1d35be6e4a75a463c1f23becab495f
luizogr <luizpkk@yahoo.com.br>
2025-11-29 22:30:18 -0300

    Tentanndo passando a role

--- src/main/java/com/ufvjm/estagios/controllers/AuthController.java
+++ src/main/java/com/ufvjm/estagios/controllers/AuthController.java
@@ -63,7 +63,7 @@ public class AuthController {
 
         if(passwordEncoder.matches(body.password(), usuario.getSenha())) {
             String token = this.tokenService.generateToken(usuario);
-            return ResponseEntity.ok(new ResponseDTO(usuario.getNome(), token, usuario.getRole().name()));
+            return ResponseEntity.ok(new ResponseDTO(usuario.getNome(), token, usuario.getRole()));
         }
         return ResponseEntity.badRequest().build();
     }
--- src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
+++ src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
@@ -4,8 +4,8 @@ import com.ufvjm.estagios.entities.enums.Role;
 
 import java.util.UUID;
 
-public record ResponseDTO (String name, String token, UUID profileId, String role){
-    public ResponseDTO(String name, String token, String role) {
+public record ResponseDTO (String name, String token, UUID profileId, Role role){
+    public ResponseDTO(String name, String token, Role role) {
         this(name, token, null, role);
     }
 }
