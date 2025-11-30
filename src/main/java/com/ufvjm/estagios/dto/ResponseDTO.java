02db36205c809637fc8638d51a77a261eeb60e9a
luizogr <luizpkk@yahoo.com.br>
2025-11-29 22:20:39 -0300

    Agora acho que vai

--- src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
+++ src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
@@ -4,8 +4,8 @@ import com.ufvjm.estagios.entities.enums.Role;
 
 import java.util.UUID;
 
-public record ResponseDTO (String name, String token, UUID profileId, Role role){//Conferir quais dados o front precisa
-    public ResponseDTO(String name, String token) {
-        this(name, token, null, null);
+public record ResponseDTO (String name, String token, UUID profileId, String role){//Conferir quais dados o front precisa
+    public ResponseDTO(String name, String token, String role) {
+        this(name, token, null, role);
     }
 }
