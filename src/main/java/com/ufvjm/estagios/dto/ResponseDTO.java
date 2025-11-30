b61f67ef59db94cb0897ef2f0e89590e364e31af
luizogr <luizpkk@yahoo.com.br>
2025-11-29 22:04:13 -0300

    Adicionado role do usuario na resposta do login

--- src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
+++ src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
@@ -1,9 +1,11 @@
 package com.ufvjm.estagios.dto;
 
+import com.ufvjm.estagios.entities.enums.Role;
+
 import java.util.UUID;
 
-public record ResponseDTO (String name, String token, UUID profileId){//Conferir quais dados o front precisa
+public record ResponseDTO (String name, String token, UUID profileId, Role role){//Conferir quais dados o front precisa
     public ResponseDTO(String name, String token) {
-        this(name, token, null);
+        this(name, token, null, null);
     }
 }
