c4c394f05573d18a72aa759870c432f23467f705
luizogr <luizpkk@yahoo.com.br>
2025-11-29 22:54:30 -0300

    liberando todas asa rotas no cors

--- src/main/java/com/ufvjm/estagios/infra/cors/CorsConfig.java
+++ src/main/java/com/ufvjm/estagios/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") //mudar para o dominio do front
+                .allowedOriginPatterns("*") //mudar para o dominio do front
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH",  "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
