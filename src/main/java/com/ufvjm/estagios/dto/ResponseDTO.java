5d06e5ae955228ad5f5423f521862128d0ca7852
luizogr <luizpkk@yahoo.com.br>
2025-11-29 22:28:20 -0300

    Tem que ir a role agora

--- src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
+++ src/main/java/com/ufvjm/estagios/dto/ResponseDTO.java
@@ -4,7 +4,7 @@ import com.ufvjm.estagios.entities.enums.Role;
 
 import java.util.UUID;
 
-public record ResponseDTO (String name, String token, UUID profileId, String role){//Conferir quais dados o front precisa
+public record ResponseDTO (String name, String token, UUID profileId, String role){
     public ResponseDTO(String name, String token, String role) {
         this(name, token, null, role);
     }
