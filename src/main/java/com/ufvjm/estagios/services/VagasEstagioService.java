068745f20528890af8c09a7267734b387cb11f45
luizogr <luizpkk@yahoo.com.br>
2026-04-12 18:36:00 -0300

    adicionado listar as vagas de estagio

--- src/main/java/com/ufvjm/estagios/services/VagasEstagioService.java
+++ src/main/java/com/ufvjm/estagios/services/VagasEstagioService.java
@@ -1,11 +1,15 @@
 package com.ufvjm.estagios.services;
 
 import com.ufvjm.estagios.dto.VagasEstagioCreateDTO;
+import com.ufvjm.estagios.dto.VagasEstagioDTO;
 import com.ufvjm.estagios.entities.VagasEstagio;
 import com.ufvjm.estagios.repositories.ProfessorRepository;
 import com.ufvjm.estagios.repositories.VagasEstagioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VagasEstagioService {
@@ -24,6 +28,19 @@ public class VagasEstagioService {
         vagasEstagio.setUrlVaga(dto.urlVaga());
         vagasEstagio.setUrlPdfDrive(dto.urlPdfDrive());
 
-        return vagasEstagioRepository.save(new VagasEstagio());
+        return vagasEstagioRepository.save(vagasEstagio);
+    }
+
+    public List<VagasEstagioDTO> listarTodasVagas(){
+        List<VagasEstagio> listaVagas = vagasEstagioRepository.findAll();
+        List<VagasEstagioDTO> listaVagasDTO = new ArrayList<>();
+        for(VagasEstagio v : listaVagas){
+            listaVagasDTO.add(converterVagasParaDTO(v));
+        }
+        return listaVagasDTO;
+    }
+
+    public VagasEstagioDTO converterVagasParaDTO(VagasEstagio vagas){
+        return new VagasEstagioDTO(vagas.getId(), vagas.getTitulo(), vagas.getDescricao(), vagas.getUrlVaga(), vagas.getUrlPdfDrive());
     }
 }
